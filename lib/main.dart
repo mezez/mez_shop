4154e7f740c92ad00d2c8b594f3e5072b4d77a98
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-08-23 12:14:20 +0100

    cart provider config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/cart.dart';
 import 'package:my_shop/screens/product_detail_screen.dart';
 import 'package:my_shop/screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
@@ -17,8 +18,15 @@ class MyApp extends StatelessWidget {
     // return ChangeNotifierProvider.value(
     //   value: (ctx) => Products(),
     //   ...
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
