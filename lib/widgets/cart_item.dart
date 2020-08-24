906ab4c81aa4c0d3d6cf17fef3631309ec4b8c28
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-08-24 23:36:15 +0100

    swipe to delete

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: FittedBox(child: Text('\$$price')),
+    //dismissible gives you swipe to remove from screen widget/feature
+    return Dismissible(
+        key: ValueKey(id),
+        background: Container(
+          color: Theme.of(context).errorColor,
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        ),
+        direction: DismissDirection
+            .endToStart, //ensure swipe only works right to left. Works all direction by default
+        onDismissed: (direction) {
+          //remove item from cart as well
+          Provider.of<Cart>(context, listen: false).removeItem(productId);
+        },
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: FittedBox(child: Text('\$$price')),
+                ),
+              ),
+              title: Text(title),
+              subtitle: Text('Total: \$${(price * quantity)}'),
+              trailing: Text('$quantity x'),
             ),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
-        ),
-      ),
-    );
+        ));
   }
 }
