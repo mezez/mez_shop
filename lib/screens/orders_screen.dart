327e26a119f8342c6f87cef53fa8b0469af27e8d
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-08-27 01:00:08 +0100

    orders screen

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:my_shop/providers/orders.dart'
+    show Orders; //we need only orders to avoid name clash
+import 'package:my_shop/widgets/order_item.dart';
+import 'package:provider/provider.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+          itemCount: orderData.orders.length,
+          itemBuilder: (ctx, i) => OrderItem(orderData.orders[i])),
+    );
+  }
+}
