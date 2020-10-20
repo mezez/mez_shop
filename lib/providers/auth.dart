fac5501492fca0c3e64a0cb6be11827920f34e8e
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-10-20 10:30:06 +0100

    login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,12 +8,21 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCiuqT5O7IbanNaDRP1xhCDttQww4yzejg";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCiuqT5O7IbanNaDRP1xhCDttQww4yzejg";
     final response = await http.post(url,
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
